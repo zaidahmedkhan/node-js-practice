ef6ea2657b5cea37de64fec2aadf2277a2677c5e
John Doe <zaidkhan200415@gmail.com>
2025-01-23 17:22:01 +0500

    destructuring

--- Java_Script_Refresher/play.js
+++ Java_Script_Refresher/play.js
@@ -1,24 +1,24 @@
-const fullName = "zaid ahmed khan";
-let age = 20;
-const hasHobbies = false;
+// const fullName = "zaid ahmed khan";
+// let age = 20;
+// const hasHobbies = false;
 
 
-const summarizeUser = (userName, age, hasHobbies) => {
+// const summarizeUser = (userName, age, hasHobbies) => {
 
-    return (
-        "Name is " + userName + ", age is " + age + " and the user has hobbies: " + hasHobbies
-    );
+//     return (
+//         "Name is " + userName + ", age is " + age + " and the user has hobbies: " + hasHobbies
+//     );
 
-}
+// }
 
-const addOne = a => a + 1;
-const add = (a, b, c) => a + b + c;
-const addRandom = () => 1 + 3;
+// const addOne = a => a + 1;
+// const add = (a, b, c) => a + b + c;
+// const addRandom = () => 1 + 3;
 
-console.log(addOne(5));
-console.log(add(1, 2, 3));
-console.log(addRandom());
-console.log(summarizeUser(fullName, age, hasHobbies));
+// console.log(addOne(5));
+// console.log(add(1, 2, 3));
+// console.log(addRandom());
+// console.log(summarizeUser(fullName, age, hasHobbies));
 
 
 
@@ -33,27 +33,48 @@ const person = {
 }
 
 
-person.greet();
+
+
+
+const printName = ({ name }) => {
+
+    console.log(name);
+
+}
+
+printName(person);
+
+const { name, age } = person;
+console.log(name, age);
+
+
+
+
+
+
+// person.greet();
 
 
 
 const hobbies = ["sports", "cooking", 20, true];
-const copiedArray = [...hobbies];
-console.log(copiedArray);
-console.log("done");
+const [hobby1, hobby2] = hobbies;
+console.log(hobby1, hobby2);
+// const copiedArray = [...hobbies];
+// console.log(copiedArray);
+// console.log("done");
 
 
-for (let hobby of hobbies) {
-    console.log(hobby);
-}
+// for (let hobby of hobbies) {
+//     console.log(hobby);
+// }
 
 
 
 
-const toArray = (...arg) => {
+// const toArray = (...arg) => {
 
-    return arg;
-}
+//     return arg;
+// }
 
 
-console.log(toArray(1, 2, 3, 4, 5, 6, "zaid", "ahmed"));
+// console.log(toArray(1, 2, 3, 4, 5, 6, "zaid", "ahmed"));
