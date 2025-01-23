90279007a482e125b711c6c1fafd97ea99056240
John Doe <zaidkhan200415@gmail.com>
2025-01-23 17:45:59 +0500

    async code and promises

--- Java_Script_Refresher/play.js
+++ Java_Script_Refresher/play.js
@@ -1,24 +1,23 @@
-// const fullName = "zaid ahmed khan";
-// let age = 20;
-// const hasHobbies = false;
+const fullName = "zaid ahmed khan";
+const hasHobbies = false;
 
 
-// const summarizeUser = (userName, age, hasHobbies) => {
+const summarizeUser = (userName, age, hasHobbies) => {
 
-//     return (
-//         "Name is " + userName + ", age is " + age + " and the user has hobbies: " + hasHobbies
-//     );
+    return (
+        "Name is " + userName + ", age is " + age + " and the user has hobbies: " + hasHobbies
+    );
 
-// }
+}
 
-// const addOne = a => a + 1;
-// const add = (a, b, c) => a + b + c;
-// const addRandom = () => 1 + 3;
+const addOne = a => a + 1;
+const add = (a, b, c) => a + b + c;
+const addRandom = () => 1 + 3;
 
-// console.log(addOne(5));
-// console.log(add(1, 2, 3));
-// console.log(addRandom());
-// console.log(summarizeUser(fullName, age, hasHobbies));
+console.log(addOne(5));
+console.log(add(1, 2, 3));
+console.log(addRandom());
+console.log(summarizeUser(fullName, 20, hasHobbies));
 
 
 
@@ -45,36 +44,64 @@ const printName = ({ name }) => {
 printName(person);
 
 const { name, age } = person;
-console.log(name, age);
+console.log(name, 20);
 
 
 
 
 
 
-// person.greet();
+person.greet();
 
 
 
 const hobbies = ["sports", "cooking", 20, true];
 const [hobby1, hobby2] = hobbies;
 console.log(hobby1, hobby2);
-// const copiedArray = [...hobbies];
-// console.log(copiedArray);
-// console.log("done");
+const copiedArray = [...hobbies];
+console.log(copiedArray);
+console.log("done");
 
 
-// for (let hobby of hobbies) {
-//     console.log(hobby);
-// }
+for (let hobby of hobbies) {
+    console.log(hobby);
+}
 
 
 
 
-// const toArray = (...arg) => {
+const toArray = (...arg) => {
 
-//     return arg;
-// }
+    return arg;
+}
 
 
-// console.log(toArray(1, 2, 3, 4, 5, 6, "zaid", "ahmed"));
+console.log(toArray(1, 2, 3, 4, 5, 6, "zaid", "ahmed"));
+
+
+
+
+
+const fetchData = callBack => {
+
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            callBack("done");
+        }, 1500);
+    });
+    return promise;
+
+}
+
+setTimeout(() => {
+    console.log("Timer is done")
+    fetchData().then(text => {
+        console.log(text)
+        return fetchData;
+    }).then(text2 => {
+        console.log(text2);
+    })
+}, 2000);
+
+console.log("hello");
+console.log("hi");
